37ffd76042ff9cb08baf5ab6353955d4a865ff1e
Sanat Tudu <94187960+SANAT-01@users.noreply.github.com>
2026-06-30 19:20:19 +0530

    Time: 59 ms (92.88%), Space: 19.4 MB (48.97%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.py
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.py
@@ -1,28 +1,8 @@
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
-        cnt=[0,0,0]
-        i=j=0
-        n=len(s)
+        cnt=[-1]*3
         ans=0
-        while j<n:
-            if cnt[0]>0 and cnt[1]>0 and cnt[2]>0:
-                ans+=n-j+1
-                idx=ord(s[i])-ord('a')
-                cnt[idx]-=1
-                i+=1
-            else:
-                idx=ord(s[j])-ord('a')
-                cnt[idx]+=1
-                j+=1
-            # print(cnt,ans,i,j)
-        if cnt[0]>0 and cnt[1]>0 and cnt[2]>0:
-            ans+=n-j+1
-        idx=ord(s[i])-ord('a')
-        cnt[idx]-=1
-        i+=1
-        while i<j and cnt[0]>0 and cnt[1]>0 and cnt[2]>0:
-            ans+=1
-            idx=ord(s[i])-ord('a')
-            cnt[idx]-=1
-            i+=1
+        for i in range(len(s)):
+            cnt[ord(s[i])-ord('a')]=i
+            ans+=1+min(cnt)
         return ans
