7dcd6e134034ff6d51bfabc88cc0f0fd00dd920b
Sanat Tudu <94187960+SANAT-01@users.noreply.github.com>
2026-07-01 21:16:10 +0530

    Time: 187 ms (17.45%), Space: 19.9 MB (63.74%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.py
+++ 0214-shortest-palindrome/0214-shortest-palindrome.py
@@ -3,9 +3,12 @@ class Solution:
         i=0
         n=len(s)
         maxi=0
+        arr=''
+        rev=''
         while i<n:
-            arr=s[:i+1]
-            if arr==arr[::-1]:
+            arr=s[i]+arr
+            rev+=s[i]
+            if rev==arr:
                 maxi=i+1
             i+=1
         return s[maxi:][::-1]+s
