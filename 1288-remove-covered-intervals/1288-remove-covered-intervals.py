52f884aef767848b6e41806ca90f50fd70a493a8
Sanat Tudu <94187960+SANAT-01@users.noreply.github.com>
2026-07-06 21:05:30 +0530

    Time: 6 ms (29.78%), Space: 19.6 MB (47.88%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.py
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.py
@@ -3,13 +3,12 @@ class Solution:
         i.sort()
         start=i[0][0]
         end=i[0][1]
-        ans=[]
+        cnt=0
         for j in i[1:]:
             if j[1]>end:
                 if j[0]>start:
-                    ans.append([start,end])
+                    cnt+=1
                 start,end=j
             else:
                 continue
-        ans.append([start,end])
-        return len(ans)
+        return cnt+1
