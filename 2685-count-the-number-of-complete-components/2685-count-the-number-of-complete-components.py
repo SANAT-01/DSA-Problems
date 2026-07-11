ebe31f718db4dea98983d8da3a2d9221d1eeb6e1
Sanat Tudu <94187960+SANAT-01@users.noreply.github.com>
2026-07-11 10:38:07 +0530

    Time: 55 ms (35.4%), Space: 19.9 MB (48.6%) - LeetHub

---
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.py
@@ -0,0 +1,32 @@
+from collections import defaultdict
+
+class Solution:
+    def countCompleteComponents(self, n: int, edges: List[List[int]]) -> int:
+        adj=defaultdict(list)
+        for i,j in edges:
+            adj[i].append(j)
+            adj[j].append(i)
+        print(adj)
+        visited=[0]*n
+        def traverse(node):
+            if node not in adj:
+                return True
+            queue=[node]
+            visited[node]=1
+            ed=len(adj[node])
+            nodes=1
+            while queue:
+                curr_node=queue.pop()
+                for ng in adj[curr_node]:
+                    if not visited[ng]:
+                        queue.append(ng)
+                        visited[ng]=1
+                        ed+=len(adj[ng])
+                        nodes+=1
+            print(ed//2==nodes*(nodes-1)//2)
+            return ed//2==nodes*(nodes-1)//2
+        cnt=0
+        for i in range(n):
+            if not visited[i] and traverse(i):
+                cnt+=1
+        return cnt
