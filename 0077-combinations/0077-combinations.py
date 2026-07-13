772a446509b6acf5de69791187cf661bcfbaae29
Sanat Tudu <94187960+SANAT-01@users.noreply.github.com>
2026-07-13 22:19:17 +0530

    Time: 104 ms (74.89%), Space: 61.3 MB (52.83%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,15 +1,15 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        ans=[]
-        def solve(arr,i,l):
-            if l==k:
-                ans.append(arr[:])
-                return
-            if i>n:
-                return
-            arr.append(i)
-            solve(arr,i+1,l+1)
-            arr.pop()
-            solve(arr,i+1,l)
-        solve([],1,0)
-        return ans
+        res = []
+        def dfs(i, curr):
+            if len(curr) == k:
+                res.append(curr.copy())
+                return 
+            if i > n:
+                return 
+            for j in range(i, n+1):
+                curr.append(j)
+                dfs(j+1,curr)
+                curr.pop()
+        dfs(1,[])
+        return res
