71a5edea357afc80db4ca117a9638f4e69a39613
Sanat Tudu <94187960+SANAT-01@users.noreply.github.com>
2026-08-03 14:02:31 +0530

    Time: 0 ms (100%), Space: 19.3 MB (57.27%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,12 +1,10 @@
 class Solution:
-    def predictTheWinner(self, A: List[int]) -> bool:
-        n = len(A)
-        if ~n & 1: return True
-
-        @cache
-        def maxDiff(i: int, j: int) -> int:
-            if i == j: return A[i]
-            return max(A[i] - maxDiff(i + 1, j),
-                       A[j] - maxDiff(i, j - 1))
-
-        return maxDiff(0, n - 1) >= 0
+    def predictTheWinner(self, nums: List[int]) -> bool:
+        n = len(nums)
+        if n % 2 == 0: 
+            return True
+        dp = list(nums)
+        for i in range(n - 2, -1, -1):
+            for j in range(i + 1, n):
+                dp[j] = max(nums[i] - dp[j], nums[j] - dp[j - 1])
+        return dp[-1] >= 0
