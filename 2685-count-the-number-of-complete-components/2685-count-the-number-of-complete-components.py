b52b21ce51ff7e909c27584292611dac90c653cc
Sanat Tudu <94187960+SANAT-01@users.noreply.github.com>
2026-08-11 23:29:51 +0530

    Time: 58 ms (27.46%), Space: 19.7 MB (69.12%) - LeetHub

--- 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.py
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.py
@@ -3,31 +3,31 @@ from collections import defaultdict
 class Solution:
     def countCompleteComponents(self, n: int, edges: List[List[int]]) -> int:
         adj=defaultdict(list)
-        cntr=defaultdict(int)
         for i,j in edges:
             adj[i].append(j)
-            cntr[i]+=1
             adj[j].append(i)
-            cntr[j]+=1
-        visited=[0]*n
         def traverse(node):
-            if node not in adj:
-                return True
             queue=[node]
-            visited[node]=1
-            ed=len(adj[node])
-            nodes=1
+            visited=set()
+            visited.add(node)
             while queue:
-                curr_node=queue.pop()
-                for ng in adj[curr_node]:
-                    if not visited[ng]:
+                node=queue.pop()
+                for ng in adj[node]:
+                    if ng not in visited:
                         queue.append(ng)
-                        visited[ng]=1
-                        ed+=cntr[ng]
-                        nodes+=1
-            return ed//2==nodes*(nodes-1)//2
-        cnt=0
+                        visited.add(ng)
+            return visited
+        seen=[]
+        ans=0
         for i in range(n):
-            if not visited[i] and traverse(i):
-                cnt+=1
-        return cnt
+            if i not in seen:
+                arr=traverse(i)
+                print(arr)
+                sm=0
+                lg=len(arr)-1
+                for node in arr:
+                    sm+=len(adj[node])
+                if lg*(lg+1)//2==sm//2:
+                    ans+=1
+                seen.extend(arr)
+        return ans
