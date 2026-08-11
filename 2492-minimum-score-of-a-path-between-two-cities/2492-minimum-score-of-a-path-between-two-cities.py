0d5023d24bd1ce32fa1232cc66b2439327dfbeee
Sanat Tudu <94187960+SANAT-01@users.noreply.github.com>
2026-08-11 22:39:39 +0530

    Time: 244 ms (14.52%), Space: 75.2 MB (32.51%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
@@ -1,17 +1,20 @@
+from collections import defaultdict
+
 class Solution:
     def minScore(self, n: int, roads: list[list[int]]) -> int:
-        root = list(range(n + 1))
-
-        def find(i: int) -> int:
-            root[i] = find(root[i]) if root[i] != i else i
-            return root[i]
-
-        for x, y, _ in roads:
-            root[find(x)] = find(y)
-
-        res, g1 = 10001, find(1)
-        for x, _, d in roads:
-            if find(x) == g1:
-                res = min(res, d)
-
-        return res
+        mini=float('inf')
+        queue=[1]
+        visited=set()
+        visited.add(1)
+        adj=defaultdict(list)
+        for i,j,d in roads:
+            adj[i].append((j,d))
+            adj[j].append((i,d))
+        while queue:
+            node=queue.pop()
+            for ng,d in adj[node]:
+                mini=min(mini,d)
+                if ng not in visited:
+                    visited.add(ng)
+                    queue.append(ng)
+        return mini
