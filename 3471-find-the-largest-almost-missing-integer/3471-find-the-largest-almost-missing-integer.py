c6a5037f46e601a2a32a986f0a3baac0cae94836
Sanat Tudu <94187960+SANAT-01@users.noreply.github.com>
2026-08-18 19:32:57 +0530

    Time: 0 ms (100%), Space: 19.5 MB (9.02%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.py
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.py
@@ -1,26 +1,10 @@
 class Solution:
-    def largestInteger(self, nums: List[int], k: int) -> int:
-        n=len(nums)
-        arr=[0]*n
-        dic={}
-        for i in range(k):
-            dic[nums[i]]=dic.get(nums[i],0)+1
-        for i in range(k):
-            if dic[nums[i]]==1:
-                arr[i]+=1
-        i+=1
-        while i<n:
-            dic[nums[i-k]]-=1
-            dic[nums[i]]=dic.get(nums[i],0)+1
-            for j in range(i-k+1,i+1):
-                if dic[nums[j]]==1:
-                    arr[j]+=1
-            i+=1
-        rep={}
-        for i in nums:
-            rep[i]=rep.get(i,0)+1
-        ans=-1
-        for i in range(n):
-            if arr[i]*rep[nums[i]]==1 or k==n:
-                ans=max(ans,nums[i])
-        return ans
+    def largestInteger(self, A: List[int], k: int) -> int:
+        f = [0] * 51
+        for x in A:
+            f[x] += 1
+        res, n = -1, len(A)
+        for i, c in enumerate(A):
+            if k == n or (f[c]==1 and (k==1 or not i or i+1==n)):
+                res = max(res, c)
+        return res
