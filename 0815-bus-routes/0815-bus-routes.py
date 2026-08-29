a557f26b6095c08ae4937f0fcd59441f15abaceb
Sanat Tudu <94187960+SANAT-01@users.noreply.github.com>
2026-08-29 23:51:42 +0530

    Time: 115 ms (46.42%), Space: 63.4 MB (5.06%) - LeetHub

--- 0815-bus-routes/0815-bus-routes.py
+++ 0815-bus-routes/0815-bus-routes.py
@@ -1,17 +1,19 @@
 class Solution:
     def numBusesToDestination(self, routes, S, T):
-        to_routes = collections.defaultdict(set)
-        for i, route in enumerate(routes):
-            for j in route:
-                to_routes[j].add(i)
-        bfs = [(S, 0)]
-        seen = set([S])
-        for stop, bus in bfs:
-            if stop == T: return bus
-            for i in to_routes[stop]:
-                for j in routes[i]:
-                    if j not in seen:
-                        bfs.append((j, bus + 1))
-                        seen.add(j)
-                routes[i] = []  # seen route
+        route_number=defaultdict(set)
+        for idx,route in enumerate(routes):
+            for i in route:
+                route_number[i].add(idx)
+        bfs=[(S,0)]
+        seen=set()
+        seen.add(S)
+        for node,d in bfs:
+            if node==T:
+                return d
+            for lvl in route_number[node]:
+                for i in routes[lvl]:
+                    if i not in seen:
+                        seen.add(i)
+                        bfs.append((i,d+1))
+                routes[lvl]=[]
         return -1
