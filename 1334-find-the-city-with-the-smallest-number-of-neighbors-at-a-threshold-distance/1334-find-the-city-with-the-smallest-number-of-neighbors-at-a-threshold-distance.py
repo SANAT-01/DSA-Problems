cd430456b1eabd60e684eac2bf886a9cf4a3febc
Sanat Tudu <94187960+SANAT-01@users.noreply.github.com>
2026-09-01 15:29:00 +0530

    Time: 707 ms (11.17%), Space: 20.4 MB (45.47%) - LeetHub

---
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
@@ -0,0 +1,28 @@
+class Solution:
+    def findTheCity(self, n: int, edges: List[List[int]], thres: int) -> int:
+        adj=defaultdict(list)
+        for i,j,d in edges:
+            adj[i].append((j,d))
+            adj[j].append((i,d))
+        def traverse(s):
+            seen=set([s])
+            dist=[float('inf')]*n
+            dist[s]=0
+            q=[(s,0)]
+            while q:
+                node,t=q.pop(-1)
+                for ng,d in adj[node]:
+                    if t+d<dist[ng] or ng not in seen:
+                        if t+d<=thres:
+                            seen.add(ng)
+                            q.append((ng,d+t))
+                        dist[ng]=d+t
+            return len(seen)-1
+        mini=float('inf')
+        ans=[]
+        for i in range(n):
+            res=traverse(i)
+            if mini>=res:
+                ans.append(i)
+                mini=res
+        return ans[-1]
