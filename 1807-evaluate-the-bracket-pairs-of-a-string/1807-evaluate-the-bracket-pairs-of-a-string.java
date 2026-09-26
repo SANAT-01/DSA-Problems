01c9e84220919504a21c26b0673931e96adb49b6
Sanat Tudu <94187960+SANAT-01@users.noreply.github.com>
2026-09-26 17:31:07 +0530

    Time: 201 ms (5.43%), Space: 116.4 MB (5.43%) - LeetHub

---
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.java
@@ -0,0 +1,37 @@
+class Solution {
+    public String evaluate(String s, List<List<String>> knowledge) {
+        ArrayList<String> arr = new ArrayList<>();
+        int l=0;
+        int r=0;
+        HashMap<String, String> k = new HashMap<>();
+        for (int idx=0; idx<knowledge.size(); idx++){
+            k.put(knowledge.get(idx).get(0), knowledge.get(idx).get(1));
+        }
+        System.out.println(k);
+        while (r<s.length()){
+            if (s.charAt(r)=='(' && r>l){
+                while (s.charAt(l)=='('){
+                    l++;
+                }
+                arr.add(s.substring(l,r));
+                l=r;
+            }
+            else if (s.charAt(r)==')'){
+                while (s.charAt(l)=='('){
+                    l++;
+                }
+                String val=s.substring(l,r);
+                arr.add(k.getOrDefault(val,"?"));
+                l=r+1;
+            } 
+            r+=1;
+        }
+        arr.add(s.substring(l,r));
+        System.out.println(arr);
+        String ans="";
+        for (String x : arr){
+            ans+=x;
+        }
+        return ans;
+    }
+}
